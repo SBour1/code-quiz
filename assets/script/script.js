8aee028898bbf66f49b8966cdd70ab92fea75fe4
Steve Bour <bourpower@gmail.com>
2021-12-19 12:43:42 -0500

    built questions and answer buttons, added button removal

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,25 +13,48 @@ function timer() {
             alert("Time's Up!")
         }
     }, 1000);
+    startButton.remove();
 
 }
 
 startButton.addEventListener('click', function gameStart() {
+    var score = [];
     timer();
     questionOne();
-    questionTwo();
-    questionThree();
-    questionFour();
-    questionFive();
+    // questionTwo();
+    // questionThree();
+    // questionFour();
+    // questionFive();
+    // secondsLeft = score
+    localStorage.setItem("score", score);
 })
 
 function questionOne() {
     var question = document.createElement("h3");
-    var answers = document.createElement("li")
-    question.textContent = "What coding language creates the frame of the web page?";
-    allQuestion.appendChild(question)
-    answers = ["HTML", "CSS", "Javascript", "Python"]
-    allAnswer.appendChild(answers)
+    var choiceOne = document.createElement("button");
+    var choiceTwo = document.createElement("button");
+    var choiceThree = document.createElement("button");
+    var choiceFour = document.createElement("button")
+    question.innerHTML = "What coding language creates the frame of the web page?";
+    allQuestion.appendChild(question);
+    choiceOne.innerHTML = "HTML";
+    choiceTwo.innerHTML = "CSS";
+    choiceThree.innerHTML = "Javascript";
+    choiceFour.innerHTML = "Python";
+    allAnswer.appendChild(choiceOne);
+    allAnswer.appendChild(choiceTwo);
+    allAnswer.appendChild(choiceThree);
+    allAnswer.appendChild(choiceFour);
+
+    allAnswer.addEventListener("click", function () {
+        if (choiceOne) {
+            console.log("correct");
+        } else {
+            console.log("wrong");
+            secondsLeft = secondsLeft - 20;
+        }
+    });
+
 }
 
 function questionTwo() {
@@ -39,7 +62,7 @@ function questionTwo() {
     var answers = document.createElement("li")
     question.textContent = "";
     allQuestion.appendChild(question)
-    answers = []
+    answers.innerHTML = []
     allAnswer.appendChild(answers)
 }
 
@@ -48,7 +71,7 @@ function questionThree() {
     var answers = document.createElement("li")
     question.textContent = "";
     allQuestion.appendChild(question)
-    answers = []
+    answers.innerHTML = []
     allAnswer.appendChild(answers)
 }
 
@@ -57,7 +80,7 @@ function questionFour() {
     var answers = document.createElement("li")
     question.textContent = "";
     allQuestion.appendChild(question)
-    answers = []
+    answers.innerHTML = []
     allAnswer.appendChild(answers)
 }
 
@@ -66,6 +89,6 @@ function questionFive() {
     var answers = document.createElement("li")
     question.textContent = "";
     allQuestion.appendChild(question)
-    answers = []
+    answers.innerHTML = []
     allAnswer.appendChild(answers)
 }
